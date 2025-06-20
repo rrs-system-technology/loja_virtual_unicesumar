927e7d22983db31bba7355a16cca1adf14ced907
Rivaldo Roberto <rrs.sistema@gmail.com>
2025-06-19 22:12:36 -0300

    Ajustes no login

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../database/database.dart';
 import '../models/models.dart';
 import '../common/common.dart';
 
@@ -15,7 +16,53 @@ class AuthService {
       final Map<String, dynamic> data = json.decode(response.body);
       return LoginResponseModel.fromJson(data);
     } else {
+      // Tentativa de buscar usuário localmente
+      final localUser = await getUserByNameAndPassword(request.username, request.password);
+      if (localUser != null) {
+        return LoginResponseModel(
+          token:
+              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXIiOiJqb2huZCIsImlhdCI6MTc1MDM3NzI4NH0.weCwGHqmhHxCcS7F99jmqpwFlC8Ei1SMXrMLFyrGzaw',
+        );
+      }
+
+      // Caso não encontre localmente, lança exceção original
       throw Exception('Erro no login: ${response.statusCode}');
     }
+  }
+
+  Future<UserModel?> getUserByNameAndPassword(String userName, String password) async {
+    final db = await AppDatabase().database;
+    final maps = await db.query(
+      'users',
+      where: 'username = ? AND password = ?',
+      whereArgs: [userName, password],
+    );
+
+    if (maps.isNotEmpty) {
+      final map = maps.first;
+      return UserModel(
+        id: map['id'] as int,
+        email: map['email'] as String,
+        username: map['username'] as String,
+        password: map['password'] as String,
+        name: NameModel(
+          firstname: map['firstname'] as String,
+          lastname: map['lastname'] as String,
+        ),
+        address: AddressModel(
+          city: map['city'] as String,
+          street: map['street'] as String,
+          number: map['number'] as int,
+          zipcode: map['zipcode'] as String,
+          geolocation: GeolocationModel(
+            lat: map['lat'] as String,
+            long: map['long'] as String,
+          ),
+        ),
+        phone: map['phone'] as String,
+      );
+    }
+
+    return null;
   }
 }
